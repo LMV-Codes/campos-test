8743bdb43add0f8a9b69f517e544c8a23ba293dc
lmv <vazquez.leo.m@gmail.com>
2021-07-08 11:03:01 -0300

    Font mobile tweaks

--- src/components/LandingText.jsx
+++ src/components/LandingText.jsx
@@ -12,20 +12,23 @@ export const LandingText = () => {
 
   return (
     <Flex flexDir="column">
-      <Heading
-        fontWeight="light"
-        fontFamily="Roboto, sans-serif"
-        fontSize={{ base: 'medium', lg: '6xl' }}
-      >
-        OPORTUNIDAD DE POZO EN LO MEJOR DE SAN MARTÍN CENTRO
-      </Heading>
-      <br />
-      <Flex>
-        <Heading fontSize={{ base: 'medium', lg: '6xl' }}>
-          DESDE USD 69.900
+      <Flex width={{ base: '18em', lg: '100%' }}>
+        <Heading
+          fontWeight="light"
+          fontFamily="Roboto, sans-serif"
+          fontSize={{ base: '1.5em', lg: '6xl' }}
+        >
+          OPORTUNIDAD DE POZO EN LO MEJOR DE SAN MARTÍN CENTRO
         </Heading>
+      </Flex>
+      <Flex>
         <Flex alignItems="center">
-          <Heading marginLeft="0.5em" fontWeight="thin">
+          <Heading fontSize={{ base: '1.5em', lg: '6xl' }} fontFamily="roboto">
+            DESDE USD 69.900
+          </Heading>
+        </Flex>
+        <Flex alignItems="center">
+          <Heading marginLeft="0.5em" fontWeight="light" fontFamily="roboto">
             - 7mo "C"
           </Heading>
         </Flex>
@@ -33,17 +36,29 @@ export const LandingText = () => {
       <br />
       <Flex>
         <Flex bg="rgb(63,91,66)" padding="0.3em" justifyContent="center">
-          <Text color="rgb(207,185,22)" letterSpacing="5px">
+          <Text
+            color="rgb(207,185,22)"
+            letterSpacing={{ base: '2px', lg: '5px' }}
+            fontSize={{ base: '0.8em', lg: 'xl' }}
+          >
             ANTICIPO USD 30.000 + 48 CUOTAS EN PESOS O EN DÓLARES
           </Text>
         </Flex>
       </Flex>
       <br />
-      <Heading>UNIDADES DE 2 Y 3 AMBIENTES</Heading>
-      <Text>EMPRENDIMIENTO PREMIUM</Text>
+      <Heading fontSize={{ base: '1.4em', lg: '4xl' }} letterSpacing="1.5px">
+        UNIDADES DE 2 Y 3 AMBIENTES
+      </Heading>
+      <Text fontSize={{ base: '1.4em', lg: '4xl' }}>
+        EMPRENDIMIENTO PREMIUM
+      </Text>
       <br />
       {perks.map((perk, index) => (
-        <Text textTransform="uppercase" key={index}>
+        <Text
+          textTransform="uppercase"
+          key={index}
+          fontSize={{ base: '1.4em', lg: '3xl' }}
+        >
           - {perk}
         </Text>
       ))}
